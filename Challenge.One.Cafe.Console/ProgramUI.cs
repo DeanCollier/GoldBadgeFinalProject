87fd7f49fcf03f569caae288d6e269aef7ced258
Dean Collier <daccollier@gmail.com>
2021-07-08 21:08:11 -0400

    finished first challenge for now, it is in a working state

--- Challenge.One.Cafe.Console/ProgramUI.cs
+++ Challenge.One.Cafe.Console/ProgramUI.cs
@@ -16,6 +16,7 @@ namespace ChallengeOneCafe_Console
 
         public void Start()
         {
+            Seed();
             RunMenu();
         }
 
@@ -41,19 +42,18 @@ namespace ChallengeOneCafe_Console
         private void OpenMenuItem(string userInput)
         {
             Console.Clear();
-            int userChoice = int.Parse(userInput);
-            switch (userChoice)
+            switch (userInput)
             {
-                case 1:
+                case "1":
                     AddMenuItem();
                     break;
-                case 2:
+                case "2":
                     DeleteMenuItem();
                     break;
-                case 3:
+                case "3":
                     DisplayAllMenuItems();
                     break;
-                case 4:
+                case "4":
                     isRunning = false;
                     return;
                 default:
@@ -72,78 +72,99 @@ namespace ChallengeOneCafe_Console
             decimal price = EnterNewItemPrice(mealName);
             int mealNumber = EnterNewItemNumber(mealName);
 
-
-
-
+            MenuItem newItem = new MenuItem(mealNumber, mealName, mealDesc, ingredients, price);
+            bool result = _repo.AddMenuItem(newItem);
+            if (result)
+            {
+                Console.WriteLine("Your menu item has been added! ");
+                PressKeyToReturnMainMenu();
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong, item not added.");
+                PressKeyToReturnMainMenu();
+            }
         }
         private string EnterNewItemName()
         {
             Console.WriteLine("Enter name of new menu item:");
-            string mealName = Console.ReadLine().ToLower();
-            mealName = IsThisCorrect(mealName);
+            string mealName = IsThisCorrect(Console.ReadLine().ToLower());
             return mealName;
         }
         private string EnterNewItemDescription(string mealName)
         {
-            Console.WriteLine($"Enter a description for " + mealName + ":");
-            string mealDesc = Console.ReadLine().ToLower();
-            mealDesc = IsThisCorrect(mealDesc);
+            Console.WriteLine($"Enter a description for {mealName}:");
+            string mealDesc = IsThisCorrect(Console.ReadLine().ToLower());
             return mealDesc;
         }
         private List<string> EnterNewItemIngredients(string mealName)
         {
             List<string> ingredientList = new List<string>();
-            Console.WriteLine($"Seperately enter each ingredient in " + mealName + ".  Press 'ENTER' twice when done: ");
+            Console.WriteLine($"Seperately enter each ingredient in {mealName}.  Press 'ENTER' again when done: ");
             string ingredient;
             while ((ingredient = Console.ReadLine().ToLower()) != "")
             {
                 ingredientList.Add(ingredient);
             }
-            if (!IsThisCorrect(String.Join(", ", ingredientList)))
-            {
-                EnterNewItemIngredients(mealName);
-            }
-            Console.Clear();
             return ingredientList;
         }
         private decimal EnterNewItemPrice(string mealName)
         {
-            Console.WriteLine($"How much will " + mealName + " cost?");
-            string mealNumber = Console.ReadLine().ToLower();
-            mealNumber = IsThisCorrect(mealNumber);
+            Console.WriteLine($"How much will {mealName} cost?");
+            string mealNumber = IsThisCorrect(Console.ReadLine().ToLower());
             return decimal.Parse(mealNumber);
         }
         private int EnterNewItemNumber(string mealName)
         {
-            Console.WriteLine($"Enter the menu item number for " + mealName + ":");
+            Console.WriteLine($"Enter the menu item number for {mealName}:");
             string mealNumber = Console.ReadLine().ToLower();
-            bool temp = true;
-            while (temp)
+            foreach (var menuItem in _repo.GetAllMenuItems())
             {
-                foreach (var menuItem in _repo.GetAllMenuItems())
+                if (int.Parse(mealNumber) == menuItem.MealNumber)
                 {
-                    if (int.Parse(mealNumber) == menuItem.MealNumber)
-                    {
-                        Console.WriteLine("That menu item number is already being used.\n" +
-                                          "Enter a different menu item number:");
-                        mealNumber = Console.ReadLine().ToLower();
-                    }
+                    Console.WriteLine("That menu item number is already being used.\n" +
+                                      "Enter a different menu item number:");
+                    mealNumber = Console.ReadLine().ToLower();
                 }
-                mealNumber = IsThisCorrect(mealNumber);
-                temp = false;
             }
-
+            mealNumber = IsThisCorrect(mealNumber);
             return int.Parse(mealNumber);
         }
 
         //------------------------------------------------------------------------------------------------------------------------------
         private void DeleteMenuItem()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("What menu item would you like to delete?");
+            foreach (var menuItem in _repo.GetAllMenuItems())
+            {
+                Console.WriteLine($" {menuItem.MealNumber}. {menuItem.MealName} ");
+            }
+            string input = Console.ReadLine().ToLower();
+            input = IsThisCorrect(input);
+            bool result = _repo.DeleteMenuItemByNumber(int.Parse(input));
+            if (result)
+            {
+                Console.WriteLine("Menu item has been removed");
+                PressKeyToReturnMainMenu();
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong, item not removed.");
+                PressKeyToReturnMainMenu();
+            }
         }
         private void DisplayAllMenuItems()
         {
-            throw new NotImplementedException();
+            foreach (var menuItem in _repo.GetAllMenuItems())
+            {
+                Console.WriteLine($"Item Number: {menuItem.MealNumber}\n" +
+                                  $"Item Name: {menuItem.MealName}\n" +
+                                  $"Item Description: {menuItem.MealDescription}\n" +
+                                  $"Ingredients: {string.Join(", ",menuItem.Ingredients)}\n" +
+                                  $"Price: {menuItem.Price}\n" +
+                                  $"");
+            }
+            PressKeyToReturnMainMenu();
         }
         //-----------------------------------------------------------------------------------------------------------------------------
         private string IsThisCorrect(string thingToCheck)
@@ -165,9 +186,29 @@ namespace ChallengeOneCafe_Console
         {
             Console.WriteLine("Press any key to return to the main menu: ");
             Console.ReadKey();
+            Console.Clear();
             RunMenu();
         }
+        private void Seed()
+        {
+            var mealNumber = 1;
+            var mealName = "Bull TEST-icles";
+            var mealDescription = "This is a test desription, yum!";
+            var ingredients = new List<string> { "list", "of", "test", "ingredients" };
+            var price = 10.95m;
 
+            var firstItem = new MenuItem(mealNumber, mealName, mealDescription, ingredients, price);
+            _repo.AddMenuItem(firstItem);
+
+            mealNumber = 2;
+            mealName = "In-TEST-ines";
+            mealDescription = "Wow, that's some good test puns!";
+            ingredients = new List<string> { "some", "other", "test", "ingredients" };
+            price = 12.49m;
+
+            var secondItem = new MenuItem(mealNumber, mealName, mealDescription, ingredients, price);
+            _repo.AddMenuItem(secondItem);
+        }
 
 
 
